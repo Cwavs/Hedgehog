d6f5b03eeb894cbe74ac2f20d9eae663d63eef8a
Cwavs <16854045+Cwavs@users.noreply.github.com>
2025-01-10 21:36:09 +0000

    On branch resume  Changes to be committed: 	modified:   Neural Fingerprinter.py If I knew this was just gonna be one commit, I wouldn't have branched.

--- Neural Fingerprinter.py
+++ Neural Fingerprinter.py
@@ -29,63 +29,68 @@ for file in args.audioDir.rglob(f"*.{args.format}"):
 
     #Print the current file being processed
     print(file)
-
-    #Load audio file in stereo
-    audioData, sr = load(file, mono=True, sr=16000)
-
-    #Obtain the mfcc for the audio data
-    mel = melspectrogram(
-        y=audioData,
-        sr=sr,
-        #I chose 96 mels because it seemed like the more likely of the two axes of the input array to be used for this, could be wrong, but it seems to work
-        n_mels=96,
-        #I borrowed the hop length and window length from Musly, again could be wrong, but it seems to work, thanks Musly
-        hop_length=160,
-        win_length=400,
-        n_fft=400
-    )
-
-    mel = power_to_db(mel, ref=amax).T
-
-    print(mel.shape)
-
-    segs = []
-    start = 0
-    if mel.shape[0] > 187:
-        while start+187 <= mel.shape[0]:
-            segs.append(mel[start:start+187, :])
-            start += 187
-    else:
-        segs.append(mel)
     
-    if(len(segs) != 1):
-        segs = stack(segs, axis=0)
+    #This is hacky and looks super fucking cursed. It works but I sure hope someone else comes along with a better way.
+    if Path((str(file).rsplit('.', 1)[0]) + ".csv").is_file() == False:
+
+        #Load audio file in stereo
+        audioData, sr = load(file, mono=True, sr=16000)
+
+        #Obtain the mfcc for the audio data
+        mel = melspectrogram(
+            y=audioData,
+            sr=sr,
+            #I chose 96 mels because it seemed like the more likely of the two axes of the input array to be used for this, could be wrong, but it seems to work
+            n_mels=96,
+            #I borrowed the hop length and window length from Musly, again could be wrong, but it seems to work, thanks Musly
+            hop_length=160,
+            win_length=400,
+            n_fft=400
+        )
+
+        mel = power_to_db(mel, ref=amax).T
+
+        print(mel.shape)
+
+        segs = []
+        start = 0
+        if mel.shape[0] > 187:
+            while start+187 <= mel.shape[0]:
+                segs.append(mel[start:start+187, :])
+                start += 187
+        else:
+            segs.append(mel)
+        
+        if(len(segs) != 1):
+            segs = stack(segs, axis=0)
+        else:
+            segs = segs[0][newaxis, ...]
+
+        #Resize the input array to take a stereo file
+        interpreter.resize_tensor_input(0, segs.shape)
+        interpreter.allocate_tensors()
+
+        #Input the result into the model
+        interpreter.set_tensor(inputDetails['index'], segs)
+
+        #Actually run the model
+        interpreter.invoke()
+
+        #Get the output data from the model
+        outputData = interpreter.get_tensor(outputDetails['index'])
+
+        print(outputData.shape)
+
+        outputData = outputData.mean(axis=0)
+
+        print(outputData.shape)
+        print(outputData)
+
+        #Reshape the data into a 1-D array and save it to a csv
+        savetxt(
+            str(file.with_suffix(".csv")) if not args.csvDir else f"{args.csvDir / (file.name.rsplit('.', 1)[0])}.csv",
+            outputData.reshape(-1),
+            delimiter=","
+        )
     else:
-        segs = segs[0][newaxis, ...]
-
-    #Resize the input array to take a stereo file
-    interpreter.resize_tensor_input(0, segs.shape)
-    interpreter.allocate_tensors()
-
-    #Input the result into the model
-    interpreter.set_tensor(inputDetails['index'], segs)
-
-    #Actually run the model
-    interpreter.invoke()
-
-    #Get the output data from the model
-    outputData = interpreter.get_tensor(outputDetails['index'])
-
-    print(outputData.shape)
-
-    outputData = outputData.mean(axis=0)
-
-    print(outputData.shape)
-    print(outputData)
-
-    #Reshape the data into a 1-D array and save it to a csv
-    savetxt(
-        str(file.with_suffix(".csv")) if not args.csvDir else f"{args.csvDir / (file.name.rsplit('.', 1)[0])}.csv",
-        outputData.reshape(-1),
-        delimiter=","
-    )
+        print("csv file already exists for " + file.name + ", assuming already fingerprinted and skipping.")
